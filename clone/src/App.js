1047899feb0cae8ae90b07cf0224c7fc28f68a45
JackHarperIMF <hemvayyala8@gmail.com>
2024-04-10 19:06:47 +0530

    initial

---
+++ clone/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
