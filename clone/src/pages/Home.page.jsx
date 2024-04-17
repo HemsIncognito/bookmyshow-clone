5a89eff1e05c80803352f8bd199782a1ec551972
JackHarperIMF <hemvayyala8@gmail.com>
2024-04-17 22:32:23 +0530

    pages

--- clone/src/pages/Home.page.jsx
+++ clone/src/pages/Home.page.jsx
@@ -1,22 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 //Layouts
 import deafultLayout from "../layouts/deafult.layout";
 
 //Components
-import EntertainmentCard from "../components/Entertainment/EntertainmentCard.Component";
-import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+import EntertainmentCard from "../components/Entertainment/EntertainmentCard.component";
+import PosterSlider from "../components/PosterSlider/PosterSlider.component";
 import HeroCarousel from "../components/Carousel/HeroCarousel.Component";
 
 const HomePage = () => {
   const [picks, setPicks] = useState([]);
-  const [newReleases, setNewReleases] = useState([]);
+  const [newReleases, setReleases] = useState([]);
   const [streams, setStreams] = useState([]);
+
+  useEffect(() => {
+    const TopRatedMovies = async () => {
+      const getTopRatedMovies = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=57a338ab77be4243276b97baa614ddad');
+      setPicks(getTopRatedMovies.data.results);
+    }
+    TopRatedMovies();
+  }, [])
+
+  useEffect(() => {
+    const UpcomingMovies = async () => {
+      const getUpcomingMovies = await axios.get("https://api.themoviedb.org/3/movie/now_playing api_key=57a338ab77be4243276b97baa614ddad");
+      setReleases(getUpcomingMovies.data.results);
+    }
+    UpcomingMovies();
+  }, [])
+
+  useEffect(() => {
+    const OnlineStreams = async () => {
+      const getStreams = await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=57a338ab77be4243276b97baa614ddad");
+      setStreams(getStreams.data.results);
+    };
+    OnlineStreams();
+  }, []);
+
+
+
   return (
     <>
       <HeroCarousel/>
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 my-3">Best of Entertainment</h1>
+        <EntertainmentCard/>
+      </div>
+
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <PosterSlider 
+          title = "Recommended Movies"
+          subtitle = "Top Picks for you"
+          posters= {picks}
+          isDark = {false}
+        />
+      </div>
+
+      <div className ='bg-premier py-12 h-26'>
+        <div className='container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+          <div className='hidden md:flex'>
+            <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" alt="RuPay"  className='w-full h-full'/>
+          </div>
+          <PosterSlider 
+            title = "Premiers"
+            subtitle = "Find Brand New Releases here"
+            posters= {newReleases}
+            isDark = {true}
+          />
+        </div>
+      </div>
+
+      <div className='container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3'>
+        <PosterSlider 
+          title = "Online Streaming Movies"
+          subtitletitle = "Now Available on Online Streams"
+          posters= {streams}
+          isDark = {false}
+        />
+      </div>
     </>
   )
 }
 
-export default deafultLayout(HomePage)
+export default deafultLayout(HomePage);
--- clone/src/pages/Movie.page.jsx
+++ clone/src/pages/Movie.page.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "axios";
+import Slider from "react-slick";
+
+//Layouts
 import MovieLayout from '../layouts/Movie.layout'
 
+// //Components
+// import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+// import MovieHero from "../components/MovieHero/MovieHero.Component";
+// import Cast from "../components/Cast/Cast.Component";
+
 const MoviePage = () => {
+
+  const{ id } = useParams();
+
+  // const { movie, setMovie } = useContext(MovieContext);
+
+  const [cast, setCast] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [recommendations, setRecommendations] = useState([]);
+
+  useEffect(() => {
+    const requestCast = async () => {
+      const getCast = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=b0380404025ff71ff7222783b12710df`);
+      setCast(getCast.data.cast);
+    };
+    requestCast();
+  }, [id]);
+
+  useEffect(() => {
+    const reqSimilarMovies = async () => {
+      const getSimilarMovies = await axios.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=b0380404025ff71ff7222783b12710df`);
+      setSimilarMovies(getSimilarMovies.data.results);
+    };
+    reqSimilarMovies();
+  }, [id]);
+
+  useEffect(() => {
+    const reqRecommededMovies = async () => {
+      const getRecommendations = await axios.get(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=b0380404025ff71ff7222783b12710df`);
+      setRecommendations(getRecommendations.data.results);
+    };
+    reqRecommededMovies();
+  }, [id]);
+
+  // useEffect(() => {
+  //   const reqMovie = async () => {
+  //     const getMovieDate = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=b0380404025ff71ff7222783b12710df`);
+  //     setMovie(getMovieDate.data);
+  //   };
+  //   requestMovie();
+  // }, [id]);
+
   return (
     <div>MoviePage</div>
   )
